77fe2e5e40efa6ee2a4929f4bd3806f6a2198f8e
Mark Baker <mrbaker1917@gmail.com>
2018-07-24 02:54:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.getElementById('nested').getElementsByClassName('target')[0];
+}
+
+function increaseRankBy(n) {
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < lis.length; i++) {
+    let oldNum = parseInt(lis[i].innerHTML);
+    lis[i].innerHTML = oldNum + parseInt(n);
+  }
+}
+
+function deepestChild() {
+  
+}
